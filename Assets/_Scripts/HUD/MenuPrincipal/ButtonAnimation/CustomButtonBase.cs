11a65e971cd9293a8732085d66e51ef58d05449c
MiguelRodriguesSilva <rm92358@estudante.fieb.edu.br>
2024-02-08 10:27:36 -0300

    08/02/2024 - BotoesAnimados

--- Assets/_Scripts/HUD/MenuPrincipal/ButtonAnimation/CustomButtonBase.cs
+++ Assets/_Scripts/HUD/MenuPrincipal/ButtonAnimation/CustomButtonBase.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class CustomButtonBase : MonoBehaviour
+public class CustomButtonBase : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public Animator anim;
     private void Start()
@@ -11,12 +12,12 @@ public class CustomButtonBase : MonoBehaviour
         anim.Play("OnStatic");
     }
 
-    private void OnMouseEnter()
+    void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
     {
         anim.Play("OnMove");
     }
 
-    private void OnMouseExit()
+    void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
     {
         anim.Play("OnStatic");
     }
